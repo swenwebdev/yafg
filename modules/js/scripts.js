1f52275792bf65e297143f6ad9c3c155bfc62f9d
swenwebdev <arkhon@arkhon-thinkpad.(none)>
2012-08-18 02:39:16 +0200

    optional stuff

---
+++ modules/js/scripts.js
@@ -0,0 +1,12 @@
+/* Author:
+Swen Gorschewski
+*/
+
+/* camera slider */
+jQuery(function(){			
+	jQuery('#slider_main').camera({
+		pagination: false,
+		thumbnails: false,
+		loader: 'bar'
+	});
+});
